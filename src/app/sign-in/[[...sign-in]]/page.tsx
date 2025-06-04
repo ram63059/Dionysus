f32e764a3594bc01e4ed92185d53f8c58d1e4b9d
ram63059 <21bsm038@iiitdmj.ac.in>
2025-06-04 17:48:10 +0530

    modified sign-up and sign-in pages

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,12 @@
 import { SignIn } from '@clerk/nextjs'
 
 export default function Page() {
-  return <SignIn />
+  return(
+
+    <div className='flex items-center justify-center'>
+
+
+      <SignIn />
+    </div>
+    )
 }
